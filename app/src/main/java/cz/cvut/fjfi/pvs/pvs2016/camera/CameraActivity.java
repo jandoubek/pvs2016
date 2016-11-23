a2998e0ee804a0b906d67bd94cfacaadeaf60c24
lnevaril <lukas.nevaril@seznam.cz>
2016-11-23 16:42:46 +0100

    removed unused string constants

--- app/src/main/java/cz/cvut/fjfi/pvs/pvs2016/camera/CameraActivity.java
+++ app/src/main/java/cz/cvut/fjfi/pvs/pvs2016/camera/CameraActivity.java
@@ -17,7 +17,6 @@ import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 import cz.cvut.fjfi.pvs.pvs2016.IApplicationConstants;
 import cz.cvut.fjfi.pvs.pvs2016.R;
 
