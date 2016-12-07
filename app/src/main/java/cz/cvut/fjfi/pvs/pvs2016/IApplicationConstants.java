20c918fa0c078b5ba76dbb7b25af4b09864b5aaa
fnj <F.Navrkal@gmail.com>
2016-12-07 17:00:20 +0100

    Formátovací změny

--- app/src/main/java/cz/cvut/fjfi/pvs/pvs2016/IApplicationConstants.java
+++ app/src/main/java/cz/cvut/fjfi/pvs/pvs2016/IApplicationConstants.java
@@ -4,5 +4,4 @@ public interface IApplicationConstants {
 
 	String DIRECTORY_NAME = "LectureNotes";
 
-
 }
--- app/src/main/java/cz/cvut/fjfi/pvs/pvs2016/util/JSONUtils.java
+++ app/src/main/java/cz/cvut/fjfi/pvs/pvs2016/util/JSONUtils.java
@@ -1,9 +1,6 @@
 package cz.cvut.fjfi.pvs.pvs2016.util;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import static cz.cvut.fjfi.pvs.pvs2016.util.FileUtils.getMetadataFiles;
 
 import java.io.File;
 import java.io.FileReader;
@@ -11,35 +8,38 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import static cz.cvut.fjfi.pvs.pvs2016.util.FileUtils.getMetadataFiles;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  * Created by fnj on 6.12.16.
  */
 
 public class JSONUtils {
-    private static JSONArray getMetadataRootArray(File jsonFile) {
-        JSONParser parser = new JSONParser();
-        JSONArray rootArray = null;
-        try {
-            rootArray = (JSONArray) parser.parse(new FileReader(jsonFile));
-        } catch (ParseException | IOException e) {
-            e.printStackTrace();
-        }
-        return rootArray;
-    }
+	private static JSONArray getMetadataRootArray(File jsonFile) {
+		JSONParser parser = new JSONParser();
+		JSONArray rootArray = null;
+		try {
+			rootArray = (JSONArray) parser.parse(new FileReader(jsonFile));
+		} catch (ParseException | IOException e) {
+			e.printStackTrace();
+		}
+		return rootArray;
+	}
 
-    public static ArrayList<Photo> getPhotoList(ArrayList<Photo> photoList) {
-        File metadataFiles[] = getMetadataFiles();
-        for (File metadataFile : metadataFiles) {
-            JSONArray jsonArray = getMetadataRootArray(metadataFile);
-            Iterator<JSONObject> objectIterator = jsonArray.iterator();
-            while (objectIterator.hasNext()) photoList.add(new Photo(objectIterator.next()));
-        }
-        return photoList;
-    }
+	public static ArrayList<Photo> getPhotoList(ArrayList<Photo> photoList) {
+		File metadataFiles[] = getMetadataFiles();
+		for (File metadataFile : metadataFiles) {
+			JSONArray jsonArray = getMetadataRootArray(metadataFile);
+			Iterator<JSONObject> objectIterator = jsonArray.iterator();
+			while (objectIterator.hasNext()) photoList.add(new Photo(objectIterator.next()));
+		}
+		return photoList;
+	}
 
-    public static ArrayList<Photo> getPhotoList() {
-        return getPhotoList(new ArrayList<Photo>());
-    }
+	public static ArrayList<Photo> getPhotoList() {
+		return getPhotoList(new ArrayList<Photo>());
+	}
 }
