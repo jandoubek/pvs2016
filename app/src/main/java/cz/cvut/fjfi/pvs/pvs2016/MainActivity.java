8217c525da74576e1f9e3976316426e09aa9b09a
Paruba <j.petr.paroubek@gmail.com>
2016-12-21 21:39:31 +0100

    přidání hlavního seznamu se seriemi

--- app/src/main/java/cz/cvut/fjfi/pvs/pvs2016/MainActivity.java
+++ app/src/main/java/cz/cvut/fjfi/pvs/pvs2016/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
 	private static ListView list_View;
 	private static EditText editText;
 	private static ArrayAdapter<String> adapter;
-	ArrayList<String> nameArr;
+	ArrayList<String> nameArr = new ArrayList<>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
