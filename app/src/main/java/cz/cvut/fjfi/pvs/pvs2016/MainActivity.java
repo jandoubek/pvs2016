1dbdde539589fb99d3432a1ea7bf123588eb5457
fnj <F.Navrkal@gmail.com>
2016-12-27 13:30:48 +0100

    Přidán share intent do menu - vygeneruje PDF z fotek v cache a vytvoří Intent pro sdílení.

--- app/src/main/java/cz/cvut/fjfi/pvs/pvs2016/MainActivity.java
+++ app/src/main/java/cz/cvut/fjfi/pvs/pvs2016/MainActivity.java
@@ -3,17 +3,23 @@ package cz.cvut.fjfi.pvs.pvs2016;
 import java.io.IOException;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import cz.cvut.fjfi.pvs.pvs2016.camera.CameraActivity;
+import cz.cvut.fjfi.pvs.pvs2016.util.FileUtils;
 import cz.cvut.fjfi.pvs.pvs2016.util.JSONUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+	private ShareActionProvider mShareActionProvider;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.main_menu, menu);
+
+		MenuItem shareItem = menu.findItem(R.id.toolbar_share);
+		mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+
 		return super.onCreateOptionsMenu(menu);
 	}
 
@@ -44,6 +54,17 @@ public class MainActivity extends AppCompatActivity {
 		case R.id.toolbar_settings:
 			Intent intent = new Intent(this, SettingsActivity.class);
 			startActivity(intent);
+			return true;
+		case R.id.toolbar_share:
+			Uri uriToImage = FileUtils.createPdfForSharingAndGetUri();
+			Intent shareIntent = new Intent();
+			shareIntent.setAction(Intent.ACTION_SEND);
+			shareIntent.putExtra(Intent.EXTRA_STREAM, uriToImage);
+			shareIntent.setType("application/pdf");
+			if (mShareActionProvider != null) {
+				mShareActionProvider.setShareIntent(shareIntent);
+			}
+			startActivity(Intent.createChooser(shareIntent, getResources().getText(R.string.share_title)));
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
